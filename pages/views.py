0fa47ecea6f665f741b8996e1613310321e8b49a
ndmichael <ukejemicheal@gmail.com>
2024-10-21 16:18:43 +0100

    index page view

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Wazza anti - phishing software'
+    }
+    return render(request, "pages/index.html", context)
