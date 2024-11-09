112d6516915beb07c2a0233fafdd3d3b44de9622
ndmichael <ukejemicheal@gmail.com>
2024-11-09 19:56:36 +0100

    Returned form to view

--- phishing/views.py
+++ phishing/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import EmailUploadForm
 
 
 @login_required
 def scan_email(request):
+    if request.method == 'POST':
+        form = EmailUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            # You can now parse the uploaded file for phishing analysis
+            return redirect('success_page')  # Redirect to a success or report page
+    else:
+        form = EmailUploadForm()
+
     context ={
-        "title": "user dashboard"
+        "title": "user dashboard",
+        "form": form
     }
     return render(request, "phishing/scan_email.html", context)
