f8196af0e6a05246ce03c0c133be0dd4c21c75d6
ndmichael <ukejemicheal@gmail.com>
2024-11-09 20:24:05 +0100

    form styled with crispy form

--- phishing/forms.py
+++ phishing/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 from .models import UploadedEmail
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Div, Row, BaseInput
+from crispy_bootstrap5.bootstrap5 import FloatingField, Field
 
 class EmailUploadForm(forms.ModelForm):
     class Meta:
         model = UploadedEmail
         fields = ['email_file']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.disable_csrf = True
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Field("email_file", wrapper_class='col-md-12', accept=".txt,.eml"),
+            )
+        )
