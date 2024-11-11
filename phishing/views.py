48d6275b5486809072a892bdcbc466962a375685
ndmichael <ukejemicheal@gmail.com>
2024-11-11 14:12:35 +0100

    Logic to process mail

--- phishing/views.py
+++ phishing/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import EmailUploadForm
-
+from phishing.utils.process_email_file import extract_email_content
 
 @login_required
 def scan_email(request):
     if request.method == 'POST':
         form = EmailUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.instance.user = request.user
-            form.save()
-            # You can now parse the uploaded file for phishing analysis
-            return redirect('success_page')  # Redirect to a success or report page
+            email_instance = form.save(commit=False)
+            email_instance.user = request.user
+            email_instance.save()
+
+            # Extract email details
+            # and save model
+            extract_email_content(email_instance)
+
+            return redirect('scan_email')  # Redirect to a success or report page
     else:
         form = EmailUploadForm()
 
