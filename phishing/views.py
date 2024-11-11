bc7ec459e69b6c00c95b627742ef21c9030215ed
ndmichael <ukejemicheal@gmail.com>
2024-11-11 14:45:54 +0100

    logic to handle error

--- phishing/views.py
+++ phishing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import EmailUploadForm
 from phishing.utils.process_email_file import extract_email_content
+from django.core.exceptions import ValidationError
 
 @login_required
 def scan_email(request):
@@ -10,11 +11,14 @@ def scan_email(request):
         if form.is_valid():
             email_instance = form.save(commit=False)
             email_instance.user = request.user
-            email_instance.save()
-
+            
             # Extract email details
             # and save model
-            extract_email_content(email_instance)
+            try:
+                extract_email_content(email_instance, request.FILES['email_file'])
+                email_instance.save()
+            except Exception as e:
+                 form.add_error('email_file', f"Failed to parse the email file: {e}")
 
             return redirect('scan_email')  # Redirect to a success or report page
     else:
