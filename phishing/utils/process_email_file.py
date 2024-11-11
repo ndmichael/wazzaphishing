0a957b1c0404dbb6f63bbdd9e753bcbd41074e34
ndmichael <ukejemicheal@gmail.com>
2024-11-11 14:10:20 +0100

    utilities function added

---
+++ phishing/utils/process_email_file.py
@@ -0,0 +1,15 @@
+import mailparser
+import os
+from django.conf import settings
+
+def extract_email_content(email_instance):
+    email_file_path = os.path.join(settings.MEDIA_ROOT, email_instance.email_file.path)
+    parsed_email = mailparser.parse_from_file(email_file_path)
+    
+    # Extract relevant data
+    email_instance.extracted_subject = parsed_email.subject
+    email_instance.extracted_sender = parsed_email.from_[0][1]  # Extract sender email
+    email_instance.extracted_body = parsed_email.body
+    
+    # Save the parsed data
+    email_instance.save()
