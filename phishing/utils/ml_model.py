61f5fd60359e7d73a94f35801ad0048e281d0df3
ndmichael <ukejemicheal@gmail.com>
2024-11-18 10:12:32 +0100

    ML model added

---
+++ phishing/utils/ml_model.py
@@ -0,0 +1,23 @@
+import joblib
+import os
+import nltk
+
+# Define the path to the model files
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "ml_models", "phishing_model.joblib")
+VECTORIZER_PATH = os.path.join(BASE_DIR, "ml_models", "vectorizer.joblib")
+
+# Load the model and vectorizer
+try:
+    phishing_model = joblib.load(MODEL_PATH)
+    vectorizer = joblib.load(VECTORIZER_PATH)
+except FileNotFoundError as e:
+    phishing_model = None
+    vectorizer = None
+    print(f"Error loading model or vectorizer: {e}")
+
+def preprocess_text(text):
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    tokens = nltk.word_tokenize(text.lower())
+    tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
+    return ' '.join(tokens)
