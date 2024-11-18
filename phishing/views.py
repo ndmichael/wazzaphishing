99898920b74951e3d09d2007c53b095962324b47
ndmichael <ukejemicheal@gmail.com>
2024-11-18 10:13:34 +0100

    Phishing logic updated

--- phishing/views.py
+++ phishing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import EmailUploadForm
+from phishing.utils.ml_model import vectorizer, phishing_model
 from phishing.utils.process_email_file import extract_email_content
 from django.core.exceptions import ValidationError
 
@@ -19,6 +20,8 @@ def scan_email(request):
                 email_instance.save()
             except Exception as e:
                  form.add_error('email_file', f"Failed to parse the email file: {e}")
+
+            vectorized_text = vectorizer.transform([processed_text])
 
             return redirect('scan_email')  # Redirect to a success or report page
     else:
