a32169f5ccea60ae3dd43d2db9e8afb8bc878139
ndmichael <ukejemicheal@gmail.com>
2024-11-20 15:48:32 +0100

    url added for phishing report

--- phishing/urls.py
+++ phishing/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import scan_email
+from .views import scan_email, analysis_report
 
 urlpatterns = [
     path('scan/email/', scan_email, name="scan_email"),
+    path('email/reports/', analysis_report, name="phishing_report"),
 ]
