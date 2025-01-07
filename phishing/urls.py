08bba6661384d40977f675d75cb0daadb5dae111
ndmichael <ukejemicheal@gmail.com>
2025-01-07 13:47:25 +0100

    user settings url

--- phishing/urls.py
+++ phishing/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import scan_email, analysis_report
+from .views import scan_email, analysis_report, user_settings
 
 urlpatterns = [
     path('scan/email/', scan_email, name="scan_email"),
     path('email/reports/', analysis_report, name="phishing_report"),
+    path('dashboard/profile/settings', user_settings, name="user_settings"),
 ]
