1de916bbfc4b754349dfa83507b79080ec6ce807
ndmichael <ukejemicheal@gmail.com>
2025-01-08 14:37:04 +0100

    return total phishing check to view

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from phishing.models import PhishingReport
 
 
 @login_required
 def user_dashboard(request):
+    total_phishing = PhishingReport.objects.all().count
     context ={
-        "title": "user dashboard"
+        "title": "user dashboard",
+        "total_phishing" :total_phishing
     }
     return render(request, "users/user_dashboard.html", context)
 
