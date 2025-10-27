f14fed7ab71fa4bc1f42e99be57b2d7a3d23a8a1
ndmichael <ukejemicheal@gmail.com>
2025-10-27 16:05:10 +0100

    changed the title

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Wazza anti - phishing software'
+        'title': 'Ruqayya anti - phishing software'
     }
     return render(request, "pages/index.html", context)
